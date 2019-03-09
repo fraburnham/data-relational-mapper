a937a108cddd16854e1831a76a62dc1bdfd0b363
Frank Burnham <frank.e.burnham@gmail.com>
2019-03-09 16:04:01 -0500

    Initial types interface

---
+++ types/interface.py
@@ -0,0 +1,18 @@
+
+# not sure imports belong in here for literate reasons, seems like the
+# tangler should round them up and put them at the top of a tangled file
+from abc import abstractmethod
+
+
+class Type:
+    @abstractmethod
+    def serialize(value):
+        pass
+
+    @abstractmethod
+    def deserialize(value):
+        pass
+
+    @abstractmethod
+    def query_prepare(value, args):
+        pass
