6d7bf4e699667eb1d1a0ebed72c4105c8e4f6cdf
Frank Burnham <frank.e.burnham@gmail.com>
2019-03-10 11:56:12 -0400

    First pass at model registration check

---
+++ sql/dict_generator.py
@@ -0,0 +1,10 @@
+
+def verify_model_registered(registry, model):
+    return model in registry
+
+def verify_models_registered(registry, query):
+    for model in query.keys():
+        if not verify_model_registered(registry, model):
+            return False
+
+    return True
